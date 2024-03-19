18565f41c7cd09287fb92495d16d001530bd25d1
OlaLunde <ola.berghlunde@gmail.com>
2024-03-19 08:47:20 +0100

    noe annet

--- Football_1v1/script.js
+++ Football_1v1/script.js
@@ -9,8 +9,53 @@ function showNav () {
     navEl.classList.toggle('show')
 }
 
+let coins = 0
 
 
+// Kode til quiz-siden
+let newbtnEl = document.getElementById('newQuestion')
+let questionEl = document.getElementById('query')
+let feedbackEl = document.getElementById('feedback')
+let answerEl = document.getElementById('answer')
+let subbtnEl = document.getElementById('submit')
+let data
 
+newQuestion.style.display = 'block'
+submit.style.display = 'none'
 
+newbtnEl.addEventListener('click', getQuestion)
 
+async function getQuestion(){
+    let url = 'https://the-trivia-api.com/api/questions?categories=sport_and_leisure&limit=1&difficulty=medium&tags=soccer'
+
+    let response = await fetch(url, {
+        headers: {
+        'Content-Type': 'application/json'
+        }
+    })
+
+    data = await response.json()
+
+    //console.log(response)
+
+    console.log(data)
+
+    questionEl.innerHTML = data[0].question
+
+    newQuestion.style.display = 'none'
+    submit.style.display = 'block'
+    }
+
+subbtnEl.addEventListener('click', answer)
+
+function answer (){
+    if (answerEl.value == data[0].correctAnswer){
+        feedbackEl.innerHTML ="correct +10 coins"
+        coins = coins+10
+    }
+    else{
+        feedbackEl.innerHTML = "wrong"
+    }
+    newQuestion.style.display = 'block'
+    submit.style.display = 'none'
+    }
